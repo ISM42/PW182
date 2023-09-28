2e712adce75cb48e646e7016a31bc983c2b78bd8
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-09-27 22:14:05 -0600

    Práctica 3: vistas, rutas, blade

---
+++ practica3/resources/views/formulario.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('titulo','Formulario') 
+    
+    @section('contenido')
+    <h1 class="display-1 text-center text-danger">FORMULARIO</h1>
+    @endsection
---
+++ practica3/resources/views/recuerdos.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla')
+    
+@section('titulo','Recuerdos') 
+
+    @section('contenido')
+    <h1 class="display-1 text-center text-danger">RECUERDOS</h1>
+    @include('partials.pagination')
+
+    @endsection
